f25fa708cf56808305f77ebdb3daddada6dd68f4
Collin <Collin.brandt1@gmail.com>
2019-02-07 12:00:54 -0600

    Formatting changes

---
+++ src/Board/Line.java
@@ -0,0 +1,40 @@
+package Board;
+
+public class Line extends AbstractBoardElement {
+
+    /////////////////////////////////////////////////////////////
+    //Fields
+    /////////////////////////////////////////////////////////////
+
+    public boolean drawn;
+
+    /////////////////////////////////////////////////////////////
+    //Constructor
+    /////////////////////////////////////////////////////////////
+
+    public Line(int row, int column) {
+        super(row, column);
+        drawn = false;
+    }
+
+    /////////////////////////////////////////////////////////////
+    //Methods
+    /////////////////////////////////////////////////////////////
+
+    public void drawLine(){
+        this.drawn = true;
+    }
+
+    @Override
+    public void print() {
+        if(drawn) {
+            if (row % 2 == 0)
+                System.out.print(" -");
+            else
+                System.out.print(" |");
+        }
+        else{
+            System.out.print("  ");
+        }
+    }
+}
